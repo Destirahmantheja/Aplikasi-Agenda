3569d09cb49ac02b2aab2b32ef076fbed1e5d660
Destirahmantheja <destitheja43742@gmail.com>
2023-03-17 15:17:01 +0700

    Aplikasi agenda lanjutan

--- app/src/main/java/com/si6b/aplikasiagenda/MyDatabaseHelper.java
+++ app/src/main/java/com/si6b/aplikasiagenda/MyDatabaseHelper.java
@@ -2,6 +2,7 @@ package com.si6b.aplikasiagenda;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -52,6 +53,17 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
 
         long eksekusi = db.insert(TABLE_NAME, null, cv);
         return eksekusi;
+    }
+
+    public Cursor bacaDataAgenda(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        String query = " SELECT * FROM " + TABLE_NAME;
+
+        Cursor varCursor = null;
+        if(db != null){
+            varCursor = db.rawQuery(query, null);
+        }
+        return varCursor;
     }
 }
 
